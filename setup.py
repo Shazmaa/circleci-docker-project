d6b24a60e9992538cfe8a59700b898eca4c7276a
Shazmaa <130198360+Shazmaa@users.noreply.github.com>
2025-04-01 23:27:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DevSecOps_Project',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pytest',
+        'requests',
+        'flask',
+    ],
+)
 
